ac945a50b58a4f3bca91c3da9c52d827005e7d4d
ExistingPerson <dedek.vlastimil2@proton.me>
2026-03-03 15:54:06 +0100

    Replace script with dracut modules (thanks to @fiftydinar)

---
+++ system_files/usr/lib/dracut/modules.d/02remove-fstab-root/module-setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+check() {
+    return 0
+}
+
+depends() {
+    return 0
+}
+
+install() {
+    inst_multiple grep sed chmod chown touch sync findmnt mv
+    inst_simple "$moddir/remove-fstab-root-initrd.sh" /lib/dracut/hooks/pre-pivot/02-remove-fstab-root.sh
+}
